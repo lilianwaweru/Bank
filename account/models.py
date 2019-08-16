acb1cb80a6b9f7bf44cfc0550c3f0acf5984d958
lilianwaweru <lilowesh.lw@gmail.com>
2019-08-16 17:14:01 +0300

    created models for history

--- account/models.py
+++ account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class History(models.Model):
+    Name = models.CharField(max_length = 20)
+    description = models.TextField(max_length=300,default=0)
+    Amount = models.IntegerField(default=0)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+    def save_History(self):
+        self.save()
+
+    def __str__(self):
+        return self.History    
