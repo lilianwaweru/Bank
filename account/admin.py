5fe0b14d723b5945ba099c155ed17941a016bd24
lilianwaweru <lilowesh.lw@gmail.com>
2019-08-20 06:34:26 +0300

    registered admin in the admin.py

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Accesses,Types,Banks,History
 
 # Register your models here.
+admin.site.register(History)
+admin.site.register(Banks)
+admin.site.register(Types)
+admin.site.register(Accesses)
