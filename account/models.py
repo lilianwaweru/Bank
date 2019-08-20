9e60fd5334e7519d4303bc5c5d5474e9f3f0ac29
lilianwaweru <lilowesh.lw@gmail.com>
2019-08-20 06:37:30 +0300

    created banks and history models

--- account/models.py
+++ account/models.py
@@ -1,15 +1,47 @@
 from django.db import models
 
 # Create your models here.
-class History(models.Model):
-    Name = models.CharField(max_length = 20)
-    description = models.TextField(max_length=300,default=0)
-    Amount = models.IntegerField(default=0)
-    date = models.DateTimeField(auto_now_add=True)
 
+class Accesses(models.Model):
+    transaction_access = models.CharField(max_length = 70)
+
+    def __str__(self):
+        return self.transaction_access 
+
+
+
+
+class Types(models.Model):
+    transaction_type = models.CharField(max_length = 70)
+
+    def __str__(self):
+        return self.transaction_type 
+
+
+
+class Banks(models.Model):
+    bank_name = models.CharField(max_length = 70)
+
+    def __str__(self):
+        return self.bank_name 
+
+
+    
+class History(models.Model):
+    name = models.CharField(max_length = 70)
+    contact = models.IntegerField(max_length = 10)
+    description = models.TextField(max_length=300,default=0)
+    amount = models.IntegerField(default=0)
+    date = models.DateTimeField(auto_now_add=True)
+    bank = models.ForeignKey(Banks,on_delete=models.CASCADE,null=True)
+    access = models.ForeignKey(Accesses,on_delete=models.CASCADE,null=True)
+    transaction = models.ForeignKey(Types,on_delete=models.CASCADE,null=True)
 
     def save_History(self):
         self.save()
 
     def __str__(self):
-        return self.History    
+        return self.name 
+
+
+
